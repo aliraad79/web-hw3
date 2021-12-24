9384fcf52b1f91c820beabca8634beaff8ac25ad
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-12-24 17:33:32 +0330

    add cors middleware

--- backend/main.go
+++ backend/main.go
@@ -76,9 +76,20 @@ func CreateToken(userid uint) (string, error) {
 	return token, nil
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+		c.Next()
+	}
+}
+
 func main() {
 
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 	note_router := router.Group("/notes")
 	// note_router.Use(JWTMiddleware())
 
