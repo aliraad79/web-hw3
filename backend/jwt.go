93290960ac2b553b40593a7335c779a3b51cdf8c
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-12-27 15:58:25 +0330

    increase token time

--- backend/jwt.go
+++ backend/jwt.go
@@ -17,7 +17,7 @@ func CreateToken(userid uint, is_admin bool) (string, error) {
 	atClaims["authorized"] = true
 	atClaims["user_id"] = userid
 	atClaims["is_admin"] = is_admin
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	atClaims["exp"] = time.Now().Add(time.Hour).Unix()
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
 	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 	if err != nil {
