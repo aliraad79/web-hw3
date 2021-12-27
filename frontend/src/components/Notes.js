e06487f3bc2aeed94089b02e10f0051389b721b9
aliraad79 <a.ahmadi.k.79@gmail.com>
2021-12-27 18:13:31 +0330

    refactore code

--- frontend/src/components/Notes.js
+++ frontend/src/components/Notes.js
@@ -1,4 +1,4 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { FaPlus } from "react-icons/fa";
 import { Button } from "react-bootstrap";
@@ -7,25 +7,24 @@ import BASE_SERVER_URL from "../consts";
 
 const Notes = (props) => {
   const [notes, setNotes] = useState([]);
+  let navigate = useNavigate();
   const token = localStorage.token;
 
   useEffect(() => {
     const getNotes = async () => {
-      const notesFromServer = await fetchAllNotes();
-      setNotes(notesFromServer);
+      const res = await fetch(`${BASE_SERVER_URL}/notes/`, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      if (res.status === 401) navigate("/");
+      else {
+        const data = await res.json();
+        setNotes(data !== "null" ? data : []);
+      }
     };
     getNotes();
   }, []);
-
-  const fetchAllNotes = async () => {
-    const res = await fetch(`${BASE_SERVER_URL}/notes/`, {
-      headers: {
-        Authorization: token,
-      },
-    });
-    const data = await res.json();
-    return data;
-  };
 
   const onDelete = (id) => {
     console.log(id);
@@ -39,7 +38,6 @@ const Notes = (props) => {
   };
 
   const updateOrAddNote = async (note) => {
-    let new_id = 0;
     if (note.id === 0) {
       fetch(`${BASE_SERVER_URL}/notes/`, {
         method: "POST",
@@ -49,23 +47,21 @@ const Notes = (props) => {
         body: JSON.stringify(note),
       })
         .then((response) => response.json())
-        .then((response) => (new_id = response.ID));
+        .then((response) =>
+          setNotes([
+            ...notes.filter((n) => n.id !== note.id),
+            { ...note, id: response.ID },
+          ])
+        );
     } else {
-      const res = await fetch(`${BASE_SERVER_URL}/notes/${note.id}`, {
+      await fetch(`${BASE_SERVER_URL}/notes/${note.id}`, {
         method: "PUT",
         headers: {
           Authorization: token,
         },
         body: JSON.stringify(note),
       });
-
-      const data = await res.json();
-      console.log(data);
     }
-    setNotes([
-      ...notes.filter((n) => n.id !== note.id),
-      { ...note, id: new_id },
-    ]);
   };
 
   const notesItems = notes.map((note) => {
