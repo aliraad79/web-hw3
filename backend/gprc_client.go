2d19bae75aa332a73250bf0a8d8dd23161db5ccc
aliraad79 <a.ahmadi.k.79@gmail.com>
2022-01-03 19:12:54 +0330

    test cache api

--- backend/gprc_client.go
+++ backend/gprc_client.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
 	"google.golang.org/grpc"
@@ -17,21 +19,35 @@ func getCacheClient() CacheServiceClient {
 
 	client := NewCacheServiceClient(conn)
 
+	getCacheKey(client, "ali")
+	setCacheKey(client, "ali", "test")
+	getCacheKey(client, "ali")
+	clearCache(client)
+
 	return client
 }
 
-// func getCacheKey(client CacheServiceClient, key string) {
-// 	response, err := client.GetKey(context.Background(), &Str{Val: key})
-// 	if err != nil {
-// 		log.Fatalf("Error when calling GetKey: %s", err)
-// 	}
-// 	fmt.Println(response)
-// }
+func getCacheKey(client CacheServiceClient, key string) {
+	response, err := client.GetKey(context.Background(), &Str{Val: key})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("response : ", response.GetVal())
+	}
+}
 
-// func setCacheKey(client CacheServiceClient, key string, value string) {
-// 	response, err := client.SetKey(context.Background(), &Cache{Key: key, Value: value})
-// 	if err != nil {
-// 		log.Fatalf("Error when calling SetKey: %s", err)
-// 	}
-// 	fmt.Println(response)
-// }
+func setCacheKey(client CacheServiceClient, key string, value string) {
+	response, err := client.SetKey(context.Background(), &Cache{Key: key, Value: value})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(response)
+}
+
+func clearCache(client CacheServiceClient) {
+	response, err := client.Clear(context.Background(), &Empty{})
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(response)
+}
