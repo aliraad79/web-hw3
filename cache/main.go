8b31ed8c70380b477009a6103c1dadd0ebc6cdf5
Broken <hamed.abdi@balad.ir>
2022-01-05 20:00:10 +0330

    add cacher

--- cache/main.go
+++ cache/main.go
@@ -1,21 +1,62 @@
 package main
 
 import (
-	"flag"
-	"net/http"
+	context "context"
+	"encoding/json"
+	"fmt"
 
-	"github.com/gin-gonic/gin"
+	"log"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
 )
 
+var cache Cache
+
+type Server struct {
+	UnimplementedCacherServer
+}
+
+func New() *Server {
+	return &Server{}
+}
+
+func (s *Server) Set(ctx context.Context, req *SetBody) (*Response, error) {
+	cache.set(req.GetKey(), req.GetValue())
+	return &Response{Result: "success"}, nil
+}
+
+func (s *Server) Get(ctx context.Context, req *GetBody) (*GetResponse, error) {
+	value := cache.get(req.GetKey())
+	return &GetResponse{Value: value}, nil
+}
+
+func (s *Server) Clear(ctx context.Context, req *Empty) (*Response, error) {
+	cache.clear()
+	return &Response{Result: "success"}, nil
+}
+
+type Configuration struct {
+	Capacity int
+	Port     string
+}
+
 func main() {
-
-	router := gin.Default()
-	cmd := flag.String("cmd", "", "")
-	flag.Parse()
-
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"S": cmd})
-	})
-
-	router.Run(":8081")
+	file, _ := os.Open("conf.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	cache = initCache(configuration.Capacity)
+	lis, err := net.Listen("tcp", "localhost:"+configuration.Port)
+	if err != nil {
+		log.Fatal("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	RegisterCacherServer(s, New())
+	s.Serve(lis)
 }
